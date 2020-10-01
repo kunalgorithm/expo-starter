10988f8e022814766a8baa3b296f1b5c17f164f1
patricia mou <patriciamou.is@gmail.com>
2020-09-30 18:35:58 -0700

    congrats quote card and assets for meditation buttons

--- screens/CongratsScreen.tsx
+++ screens/CongratsScreen.tsx
@@ -13,14 +13,18 @@ export default function CongratsScreen({
   if (screen === 0)
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>🎉 Congrats! </Text>
-        <Text style={styles.title}>Day 5 of 60 days completed </Text>
+        <Text style={styles.title}>Congrats! ✨</Text>
+        <Text style={styles.subtitle}>Day 5 of 60 days completed </Text>
         <View>
-          <Text style={styles.quote}>
-            Life is really a single player game. It's all going on in your head
-          </Text>
-          <Text style={styles.author}>Naval</Text>
+          <View style={styles.box}>
+            <Text style={styles.quote}>
+              Life is really a single player game. It's all going on in your
+              head
+            </Text>
+            <Text style={styles.author}>Naval</Text>
+          </View>
         </View>
+
         <TouchableOpacity onPress={() => setScreen(1)} style={styles.button}>
           <Text style={styles.buttonText}>Next</Text>
         </TouchableOpacity>
@@ -60,20 +64,51 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   title: {
+    fontSize: 30,
+    color: "#4A4A4A",
+    fontWeight: "bold",
+    letterSpacing: 1.5,
+    marginTop: -1,
+    fontFamily: "Calibre-Medium",
+  },
+  subtitle: {
     fontSize: 20,
     color: "#4A4A4A",
     fontWeight: "bold",
-    margin: 25,
+    letterSpacing: 1.5,
+    margin: 10,
     fontFamily: "Calibre-Medium",
   },
   quote: {
-    fontSize: 40,
+    paddingTop: 0,
+    padding: 20,
+    fontSize: 35,
     color: "#4A4A4A",
     fontWeight: "bold",
     fontFamily: "Calibre-Medium",
   },
+  box: {
+    height: 280,
+    width: 300,
+    shadowColor: "#8D8A8A",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    borderRadius: 35 / 5,
+
+    elevation: 9,
+    backgroundColor: "#fff",
+    color: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 5,
+  },
+
   author: {
-    fontSize: 20,
+    fontSize: 25,
     color: "#C4C4C4",
     fontWeight: "bold",
     fontFamily: "Calibre-Medium",
@@ -88,13 +123,17 @@ const styles = StyleSheet.create({
     padding: 15,
     width: 146,
     borderRadius: 100,
-    marginVertical: 50,
+    marginVertical: 20,
     alignItems: "center",
   },
   buttonText: {
     fontSize: 20,
     color: "#fff",
     letterSpacing: 4,
+    textTransform: "uppercase",
+    marginTop: 10,
+    marginLeft: 5,
+    alignItems: "center",
     fontFamily: "Calibre-Medium",
   },
 });

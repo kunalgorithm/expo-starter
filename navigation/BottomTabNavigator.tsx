ce41238ac8359da75a5c9f7de7416152bec0679a
Kunal <me@kunal.sh>
2020-10-03 11:13:27 -0700

    fix icon conflict

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -72,8 +72,6 @@ export default function BottomTabNavigator(user: any) {
           ),
         }}
       /> */}
-      <BottomTab.Screen
-        name="History"
 
       <BottomTab.Screen
         name="Logs"
@@ -83,8 +81,6 @@ export default function BottomTabNavigator(user: any) {
             <Image
               source={require("../assets/icons/time_machine.png")}
               style={styles.icon}
-              source={require("../assets/icons/History.png")}
-              style={{ ...styles.icon, width: styles.icon.width + 4 }}
             />
           ),
         }}

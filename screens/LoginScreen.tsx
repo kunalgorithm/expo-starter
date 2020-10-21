7416fec214bf87f596f509cea45851c52bcc7252
patricia mou <patriciamou.is@gmail.com>
2020-10-21 14:49:43 -0700

    naval

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -4,10 +4,13 @@ import { AsyncStorage, StyleSheet, Text, View } from "react-native";
 import Button from "../components/Button";
 import { Image, TextInput, TouchableOpacity } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { fetcher } from "../hooks/fetcher";
+import { fetcher, useMe } from "../hooks/fetcher";
 import { mutate } from "swr";
+import { Magic } from "@magic-sdk/react-native";
 import { useInterval } from "../hooks/useInterval";
 
+// const m = new Magic(process.env.MAGIC_KEY!);
+const m = new Magic("pk_test_4416C52A96118C14");
 export default function LoginScreen() {
   const [login, setLogin] = React.useState(false);
   const [name, setName] = React.useState("");
@@ -29,25 +32,12 @@ export default function LoginScreen() {
         return;
       } catch (error) {}
     }
-  }, 1000);
+  }, 1500);
 
   const onSubmit = async () => {
     setLoading(true);
     setError(false);
     let res;
-
-    if (usePassword) {
-      const res = await fetcher(`/api/${login ? "login" : "signup"}`, {
-        email,
-        name,
-        password,
-      });
-      if (res.data && res.data.user) {
-        await mutate("/api/me", { ...res.data.user, meditations: [] });
-        return;
-      }
-    }
-
     try {
       res = await fetcher(`/api/auth/getMagicLink`, {
         email,
@@ -69,7 +59,7 @@ export default function LoginScreen() {
         style={styles.logo}
         source={require("../assets/images/login_logo.png")}
       />
-      <m.Relayer />
+      <Relayer />
       <Text style={styles.title}></Text>
       <KeyboardAwareScrollView
         style={{ flex: 1, width: "100%" }}
@@ -80,7 +70,7 @@ export default function LoginScreen() {
             style={styles.input}
             placeholder="Name"
             placeholderTextColor="#ccc"
-            onChangeText={(text: string) => setName(text)}
+            onChangeText={(text) => setName(text)}
             value={name}
             underlineColorAndroid="transparent"
             autoCapitalize="none"
@@ -90,7 +80,7 @@ export default function LoginScreen() {
           style={styles.input}
           placeholder="E-mail"
           placeholderTextColor="#ccc"
-          onChangeText={(text: string) => setEmail(text)}
+          onChangeText={(text) => setEmail(text)}
           value={email}
           underlineColorAndroid="transparent"
           autoCapitalize="none"
@@ -101,7 +91,7 @@ export default function LoginScreen() {
             placeholderTextColor="#ccc"
             secureTextEntry
             placeholder="Password"
-            onChangeText={(text: string) => setPassword(text)}
+            onChangeText={(text) => setPassword(text)}
             value={password}
             underlineColorAndroid="transparent"
             autoCapitalize="none"
@@ -128,7 +118,7 @@ export default function LoginScreen() {
                 <TouchableOpacity onPress={() => setUsePassword(!usePassword)}>
                   <Text style={styles.footerLinktwo}>
                     {usePassword
-                      ? "Login with magic link 🔗"
+                      ? "Login with magic link 👉"
                       : "Login with password 👉"}
                   </Text>
                 </TouchableOpacity>
@@ -223,6 +213,6 @@ const styles = StyleSheet.create({
     fontFamily: "Calibre-Medium",
     fontWeight: "bold",
     fontSize: 22,
-    marginBottom: 20,
+    marginBottom: 0,
   },
 });
